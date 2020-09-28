de71ce0d73d7c67a2ccc8a06d5165aa8df88ed3f
Dhruv <55866701+dhrupatel97@users.noreply.github.com>
2020-09-27 21:26:27 -0400

    US05 and test_case

---
+++ Team4_test_Project_File.py
@@ -0,0 +1,14 @@
+import unittest
+import Team4_Project_File
+from prettytable import PrettyTable
+
+
+class fooTest(unittest.TestCase):
+
+    def test_story_us05(self):
+        """Testing User Story 05"""
+        self.assertEqual(Team4_Project_File.US05(),'US05 - Error : Individual - I1, I11 have marriage before death')
+
+
+if __name__=='__main__':
+    unittest.main()
